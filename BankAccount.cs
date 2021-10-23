3018221566e66676b29f787a838c352968b007a1
AbdulmagedALKHULAIFI <43535031+AbdulmagedALKHULAIFI@users.noreply.github.com>
2021-10-23 18:57:10 +0200

    Basic example of unit tests

---
+++ BankAccount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestExample
+{
+    public class BankAccount
+    {
+
+        private double balance;
+
+        public BankAccount()
+        {
+
+        }
+        public BankAccount(double balance)
+        {
+            this.balance = balance;
+        }
+
+        public double getBalance() { return balance; }
+
+        public void witdraw(double amount)
+        {
+
+            if (amount > balance)
+                throw new ArgumentOutOfRangeException(nameof(amount));
+
+            balance -= amount;
+        }
+
+        public void add(double amount)
+        {
+            if (amount < 0)
+                throw new ArgumentOutOfRangeException(nameof(amount));
+
+            balance += amount;
+        }
+    }
+}
